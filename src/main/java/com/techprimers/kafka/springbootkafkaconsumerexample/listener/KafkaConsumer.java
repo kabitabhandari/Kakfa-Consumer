4ed76bbea78cff08dd4dff98a673cf38bccf12a2
kabita bhandari <36861110+kabitabhandari@users.noreply.github.com>
2022-01-10 20:19:47 -0600

    Consumer code

---
+++ src/main/java/com/techprimers/kafka/springbootkafkaconsumerexample/listener/KafkaConsumer.java
@@ -0,0 +1,23 @@
+package com.techprimers.kafka.springbootkafkaconsumerexample.listener;
+
+import com.techprimers.kafka.springbootkafkaconsumerexample.model.User;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+    @KafkaListener(topics = "Kafka_Example", group = "group_id")
+    public void consume(String message) {
+        System.out.println("Consumed message: " + message);
+    }
+
+
+    @KafkaListener(topics = "kafka_topic_json", group = "group_json",   // should be same in producer and consumer
+            containerFactory = "userKafkaListenerFactory")
+    public void consumeJson(User user) {
+        System.out.println("Consumed JSON Message: " + user);
+        System.out.println(">>> department is " + user.getDept());
+        System.out.println(">>> salary is " + user.getSalary());
+        System.out.println(">>> name is " + user.getName());
+    }
+}
